0bcc79cee754d36cc73e4f6724cc85eda4a78679
ZhannaKim26 <122507467+ZhannaKim26@users.noreply.github.com>
2024-12-07 22:59:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+print('Zadacha 1:')
+#1st program
+print(9**0.5*5)
+print('Zadacha 2:')
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+print('Zadacha 3:')
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+print('Zadacha 4:')
+#4rd program
+print(int(float('123.456')*10)%10)
+
+
